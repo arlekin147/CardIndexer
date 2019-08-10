0bd30251b2009f16caa6ac8ac433e7f952dae8b0
Alexandr Pobejimov <aapobejimov@gmail.com>
2019-08-10 16:47:59 +0300

    Configurations of DataSource and DataModels were added

--- Configuration/Configuration.cs
+++ Configuration/Configuration.cs
@@ -1,11 +1,33 @@
+using System.Collections.Generic;
+using System;
+using System.IO;
+using Carbon.Toml;
 
 namespace CardIndexer.Configuration
 {
-    public class Configuration
+    public class TomlConfigurationParser : IConfigurationParser
     {
-        public Configuration(string fileName)
+        public TomlConfigurationParser(string filename)
         {
-
+            DataModels = new Dictionary<string, List<string>>();
+            var toml = Toml.Parse(new FileInfo(filename));
+            Source = toml["Source"]["googleSheetSource"];
+            foreach (var entity in toml)
+            {
+                switch (entity.Key)
+                {
+                    case "DataModel":
+                        {
+                            DataModels.Add(
+                                entity.Value["name"],
+                                new List<string>(entity.Value["mainFields"].ToArrayOf<string>()));
+                            break;
+                        }
+                }
+            }
         }
+
+        public string Source { get; private set; }
+        public Dictionary<string, List<string>> DataModels { get; private set; }
     }
 }
