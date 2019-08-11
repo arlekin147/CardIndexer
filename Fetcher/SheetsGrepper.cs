92e1e28e565818ab1312a6e467893ea94b69e6d8
Ivan Shershnev <ivankrut856@yandex.ru>
2019-08-11 13:46:35 +0200

    Logging included in Fetcher

--- Fetcher/SheetsGrepper.cs
+++ Fetcher/SheetsGrepper.cs
@@ -10,6 +10,7 @@ using System;
 
 namespace CardIndexer {
     class SheetsGrepper : IGrepper {
+        static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
         static string[] _scopes = { SheetsService.Scope.SpreadsheetsReadonly };
         static string _applicationName = "CardIndexer";
 
@@ -29,7 +30,8 @@ namespace CardIndexer {
                     "user",
                     CancellationToken.None,
                     new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
+
+                _logger.Trace("Credential file saved to: {0}", credPath);
             }
 
             _service =  new SheetsService(new BaseClientService.Initializer()
@@ -38,15 +40,17 @@ namespace CardIndexer {
                 ApplicationName = _applicationName,
             });
 
+            _logger.Trace("Service established");
         }
         public IEnumerable<List<Dictionary<string, string>>> Fetch(string sheetsId, string tableName)
         {
             // TODO to extend maximum row further Z
             var propertyValues = _service.Spreadsheets.Values.Get(sheetsId, String.Format("{0}!A{1}:Z{2}", tableName, 1, 1)).Execute().Values;
-
             if (propertyValues == null || propertyValues.Count == 0) {
+                _logger.Warn("Property row not found. No data will be acquired");
                 yield break;
             }
+            _logger.Trace("Property row found and non-empty");
 
             var propertyRow = propertyValues[0];
             int propertiesCount = propertyRow.Count;
@@ -58,8 +62,10 @@ namespace CardIndexer {
                                                                                                         group * groupSize + 2, (group + 1) * groupSize + 1));
                 var response = request.Execute();
                 if (response.Values == null || response.Values.Count == 0) {
+                    _logger.Trace("End of rows");
                     break;
                 }
+                _logger.Trace("New response in processing");
 
                 IGroupBuilder builder = new KeyValueBuilder();
                 for (int rowId = 0; rowId < response.Values.Count; rowId++)
@@ -80,17 +86,17 @@ namespace CardIndexer {
                             value = row[i].ToString();
                         }
                         
+                        if (value.Equals(""))
+                        {
+                            _logger.Warn("{0};{1} indexed cell is empty which is unexpected", recordId, i);
+                        }
                         
-                        // if (mainFields.Exists((elem) => elem.Equals(currentKey)))
-                        // {
-                        //     Console.Write("(M)");
-                        // }
-                        // Push to some dict instead
-
                         builder.AddProperty(currentKey, value);
+                        _logger.Trace("Parametes added to {0};{1} as {2}: {3}", recordId, i, currentKey, value);
                     }
                     builder.FinalizeGroup();
                 }
+                _logger.Trace("End of processing a response");
                 yield return builder.ToGroupList();
             }
             yield break;
